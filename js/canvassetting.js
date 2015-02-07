d58678fcbdbf14cb05047865a311d9568628bbc5
takuya711 <s.takuya93@gmail.com>
2015-02-08 01:54:11 +0900

    remove indextext

--- js/canvassetting.js
+++ js/canvassetting.js
@@ -9,10 +9,3 @@ function sizing(){
 	$("#main").attr({height:$("#wrapper").height()});
 	$("#main").attr({width:$("#wrapper").width()});
 }
-
-jQuery(document).ready(function(){
-    $(document).mousemove(function(e){
-        jmouseX = e.pageX;
-        jmouseY = e.pageY;
-    }); 
-})
