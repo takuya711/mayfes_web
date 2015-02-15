4784dde999e893fae57c04395ed4b163e1d1b4b6
takuya711 <s.takuya93@gmail.com>
2015-02-15 17:47:02 +0900

    mobile pjs

---
+++ js/mobile_pjs.js
@@ -0,0 +1,20 @@
+/**
+ *  ユーザーのデバイスを返す
+ *  
+ *  @return     スマホ(sp)、タブレット(tab)、その他(other)
+ *
+ */
+var getDevice = (function(){
+    var ua = navigator.userAgent;
+    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+        return 'sp';
+    }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+        return 'tab';
+    }else{
+        return 'other';
+    }
+})();
+
+if (getDevice=='sp' || getDevice=='tab'){
+    document.getElementById('pjs').innerHTML = '<canvas data-src="./processing/digits.pde"></canvas>'; 
+}
