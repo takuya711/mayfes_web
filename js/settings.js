b1743ca77300ab7943de42cc879fd3900fef035f
takuya711 <s.takuya93@gmail.com>
2015-04-19 23:31:32 +0900

    added everything

---
+++ js/settings.js
@@ -0,0 +1,10 @@
+﻿$('.nonactive').hover(
+    function () {
+        $(this).removeClass('nonactive');
+        $(this).addClass('active');
+    },
+    function () {
+        $(this).removeClass('active');
+        $(this).addClass('nonactive');
+    }
+    );
