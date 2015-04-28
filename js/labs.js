8d505929dd454100eafcd258592a5fe994783913
takuya711 <s.takuya93@gmail.com>
2015-04-28 23:37:39 +0900

    change

--- js/labs.js
+++ js/labs.js
@@ -1,8 +1,38 @@
-﻿$(".lablist").hover(
+﻿/**$(".lablist").hover(
     function () {
         $(this).children('p').show("fast");
     },
     function () {
         $(this).children('p').hide("fast");
     }
+);**/
+
+/**$(".lablist").hover(
+    function () {
+        $(this).click(
+        function () {
+            $(this).children('p').show("fast");
+        })
+    },
+    function () {
+        $(this).children('p').hide("fast");
+    }
+);**/
+
+$(".lablist").hover(
+    function () {
+        $(this).css("cursor","pointer");
+    },
+    function () {
+        $(this).css("cursor","pointer");
+    }
 );
+
+$(".lablist").on('click', function () {
+    if ($(this).children('p').css('display') == 'none') {
+        $(this).children('p').show("normal");
+    }
+    else {
+        $(this).children('p').hide("normal");
+    }
+});
