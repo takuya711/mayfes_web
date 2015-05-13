7c7b5487760f2d051112853a4bb2b72fd1445eec
takuya711 <s.takuya93@gmail.com>
2015-05-13 23:44:56 +0900

    fafdsafsa

---
+++ js/smoothscroll.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var headerHight = 170; //ヘッダーの高さ
+    $('a[href^=#]').click(function () {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - headerHight;
+        $("html, body").animate({ scrollTop: position }, 550, "swing");
+        return false;
+    });
+});
